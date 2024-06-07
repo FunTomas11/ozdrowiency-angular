eecfd585c875a517f1e484917b0896d592db989b
Artem <akrainiev@betacom.com.pl>
2024-06-07 22:46:33 +0200

    Generated primary project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },
+  {path: '', component: LandingPageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', redirectTo: ''}
 ];
---
+++ src/app/components/therapist/therapist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-therapist',
+  standalone: true,
+  imports: [],
+  templateUrl: './therapist.component.html',
+  styleUrl: './therapist.component.scss'
+})
+export class TherapistComponent {
+
+}
