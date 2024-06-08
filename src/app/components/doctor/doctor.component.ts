f0abd5737fcdc2d56938c3d9d16a9f473d9087e2
Tomas <krainievlc@gmail.com>
2024-06-08 22:07:38 +0200

    Improved doctors space

--- src/app/components/doctor/doctor.component.ts
+++ src/app/components/doctor/doctor.component.ts
@@ -1,28 +1,71 @@
-import {Component, Input} from '@angular/core';
-import {Doctor, GenericUser} from "../../models/user.model";
+import {Component, Input, OnDestroy} from '@angular/core';
+import {Doctor, GenericUser, Patient} from "../../models/user.model";
 import {UserAreaComponent} from "../user-area/user-area.component";
+import {Observable, of, Subject, takeUntil} from "rxjs";
+import {UserService} from "../../services/user.service";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {AsyncPipe, DatePipe} from "@angular/common";
+import {Form} from "../../models/form.model";
+import {MatListModule} from "@angular/material/list";
+import {MatLine} from "@angular/material/core";
 
 @Component({
   selector: 'app-doctor',
   standalone: true,
   imports: [
-    UserAreaComponent
+    UserAreaComponent,
+    AsyncPipe,
+    MatExpansionModule,
+    MatListModule,
+    MatLine,
+    DatePipe
   ],
   templateUrl: './doctor.component.html',
   styleUrl: './doctor.component.scss'
 })
-export class DoctorComponent {
+export class DoctorComponent implements OnDestroy {
   @Input() set user(userData: GenericUser) {
     this._user = userData as Doctor;
+    this._init();
   };
+
+  patients$: Observable<Patient[]> = of([]);
+  stats: Form[] = [];
 
   get user(): Doctor {
     return this._user;
   }
 
+  private _destroy$ = new Subject<void>();
   private _user!: Doctor;
 
-  constructor() {
+  constructor(private _users: UserService) {
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+
+  isPatientScored(patientId: string): boolean {
+    return this.stats.some(stat => stat.patientId === patientId);
+  }
+
+  getPatientAvgScore(patientId: string): number {
+    const patientAnswers = this.getPatientAnswers(patientId);
+    const totalScore = patientAnswers.reduce((acc, answer) => acc + answer.score, 0);
+    return totalScore / patientAnswers.length;
+  }
+
+  getPatientAnswers(patientId: string): Form[] {
+    return this.stats.filter(stat => stat.patientId === patientId);
+  }
+
+  private _init(): void {
+    this.patients$ = this._users.getDoctorsPatients(this.user.id);
+    this._users.getDoctorsStats(this.user.id)
+      .pipe(takeUntil(this._destroy$))
+      .subscribe(stats => this.stats = stats);
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
-import {GenericUser} from "../models/user.model";
+import {GenericUser, Patient} from "../models/user.model";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment.development";
+import {Form} from "../models/form.model";
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,13 @@ export class UserService {
 
   getUserDetails(userId: string): Observable<GenericUser> {
     return this._http.get<GenericUser>(`${this._apiUrl}/users/${userId}`);
+  }
+
+  getDoctorsPatients(doctorId: string): Observable<Patient[]> {
+    return this._http.get<Patient[]>(`${this._apiUrl}/users?doctorId=${doctorId}&role=patient`);
+  }
+
+  getDoctorsStats(doctorId: string): Observable<Form[]> {
+    return this._http.get<Form[]>(`${this._apiUrl}/answers?doctorId=${doctorId}`);
   }
 }
